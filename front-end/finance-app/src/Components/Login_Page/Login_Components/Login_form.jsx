6b520a7a15d210f9db520957ee57feebba0d9769
omaima.abidy <omaimaabidy@gmail.com>
2023-05-29 21:28:15 +0100

    login

--- front-end/finance-app/src/Components/Login_Page/Login_Components/Login_form.jsx
+++ front-end/finance-app/src/Components/Login_Page/Login_Components/Login_form.jsx
@@ -1,17 +1,51 @@
 import './Login_form.css';
 import logo from '../../Home_Page/Home_Components/Header/logo.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from './config/Actions';
+import axios from 'axios';
+import swal from 'sweetalert';
 export default function LoginForm(){
-    const dispatch = useDispatch();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [loginInput, setLogin] = useState({
+        username:'',
+        password:'',
+        error_list:{}
+    })
+    const handleInput = (e) =>{
+        e.persist();
+        setLogin({
+            ...loginInput, [e.target.name ]: e.target.value
+        })
+
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        dispatch(login(username, password));
+        const data = {
+            username: loginInput.username,
+            password: loginInput.password
+        }
+        console.log(data);
+        axios.post('api/login', data, {
+            headers:{
+            'content-type': 'multipart/form-data',
+            }
+          }).then(res =>{
+            if(res.status === 200){
+                localStorage.setItem('token', res.token)
+                swal('Success', res.data.message, 'success')
+                navigate('/')
+            }
+            else if(res.status === 401){
+                swal('Warning', res.data.message, 'warning')
+            }
+            else{
+                setLogin({
+                    ...loginInput,error_list : res.data.errors
+                })
+            }
+
+        });
+
       };
     return(
         <div className='Login'>
@@ -19,10 +53,10 @@ export default function LoginForm(){
                 <form className='form' onSubmit={handleSubmit}>
                     <h3>Login To Your Account</h3>
                     <div className='inputs'>
-                        <input type="text" placeholder='username' value={username}
-                            onChange={(e) => setUsername(e.target.value)}/>
-                        <input type="text" placeholder='password' value={password}
-                            onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="text" placeholder='username' name='username' onChange={handleInput} value={loginInput.username}/>
+                        <span>{loginInput.error_list.username}</span>
+                        <input type="password" placeholder='password' name='password' onChange={handleInput} value={loginInput.password}/>
+                        <span>{loginInput.error_list.password}</span>
                         <button className='btn1'>forgot password ?</button>
                         <button type="submit" className='btn2'>SIGN IN</button>
                         <i class='far fa-eye'></i>
--- front-end/finance-app/src/index.js
+++ front-end/finance-app/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reducer  from './Components/Login_Page/Login_Components/config/Reducer';
-import { legacy_createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-const store = legacy_createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
