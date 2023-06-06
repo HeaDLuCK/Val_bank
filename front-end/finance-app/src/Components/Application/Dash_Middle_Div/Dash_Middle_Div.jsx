b0de00ac6e41752df56630959fd7fb1e24b995a3
headluck <106028299+HeaDLuCK@users.noreply.github.com>
2023-06-06 01:15:52 +0100

    fixed

--- front-end/finance-app/src/Components/Application/Dash_Middle_Div/Dash_Middle_Div.jsx
+++ front-end/finance-app/src/Components/Application/Dash_Middle_Div/Dash_Middle_Div.jsx
@@ -22,73 +22,100 @@ export default function Middle_div() {
     const [balance, setBalance] = useState();
     const [recieve, setRecieve] = useState([]);
     const [deposit, setDeposit] = useState([]);
-    // const [account, setAccount] = useState(localStorage.getItem('accounts')[0]);
+    const [account, setAccount] = useState(localStorage.getItem('accounts')[0]);
     const [expenses, setExpenses] = useState([]);
 
-    // const handleInput = (e) => {
-    //     setDates({ ...dates, [e.target.name]: e.target.value })
-    //     console.log(dates);
-    // }
+    const handleInput = (e) => {
+        setDates({ ...dates, [e.target.name]: e.target.value })
+        console.log(dates);
+    }
 
-    // useEffect(() => {
-    //     axios.post(`/api/data/dashboard/${account}`, dates,
-    //         {
-    //             headers: {
-    //                 "Authorization": `Bearer ${localStorage.getItem('token')}`,
-    //                 'content-type': 'multipart/form-data'
-    //             }
-    //         })
-    //         .then(res => {
-    //             setExpenses(res.data.payload.expenses)
-    //             setBalance(res.data.payload.balance)
-    //             setRecieve(res.data.payload.recieve)
-    //             setDeposit(res.data.payload.deposit)
-    //         }).catch(err => {
-    //             console.log(err);
-    //         });
-    // }, [dates, account]);
-    // const data = {
-    //     labels: expenses.map(e => {
-    //         return [e.type]   //`${[e.type]}`
-    //     }
-    //     ),
-    //     datasets: [{
-    //         data: expenses.map(e => {
-    //             return [e.expense]
-    //         }),
-    //         backgroundColor: ['#191970', '#4682B4', '#6495ED'],
-    //     }]
+    useEffect(() => {
+        axios.post(`/api/data/dashboard/${account}`, dates,
+            {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`,
+                    'content-type': 'multipart/form-data'
+                }
+            })
+            .then(res => {
+                setExpenses(res.data.payload.expenses)
+                setBalance(res.data.payload.balance)
+                setRecieve(res.data.payload.received)
+                setDeposit(res.data.payload.deposit)
+            }).catch(err => {
+                console.log(err);
+            });
+    }, [dates, account]);
+    const data = {
+        labels: expenses.map(e => {
+            return [e.type]   //`${[e.type]}`
+        }
+        ),
+        datasets: [{
+            data: expenses.map(e => {
+                console.log(e.expense);
+                return [e.expense]
+            }),
+            backgroundColor: ['#191970', '#4682B4', '#6495ED'],
+        }]
 
-    // }
+    }
 
-    // const options = {
-    //     title: "Transactions Overview",
-    //     pieHole: 0.4,
-    //     is3D: false,
-    //     plugins: {
-    //         legend: {
-    //             display: false
-    //         }
-    //     }
-    // };
+    const options = {
+        title: "Transactions Overview",
+        pieHole: 0.4,
+        is3D: false,
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    };
 
-    //  const dataa = [
-    //     ["Year", "recieved", "deposit"],
-    //     recieve.map(e => {
-    //         return([`${e.date}`, e.sent, deposit.filter(j => j.date === e.date).map(i => i.sent)]) 
-    //     })
-    //   ];
-      
-    //      const optionss = {
-    //     chart: {
-    //       title: "Company Performance",
-    //       subtitle: "deposit, Expenses, and Profit: 2014-2017",
-    //     },
-    //   };
+    const needFilter = [
+        []
+    ].concat(recieve.length && recieve.map(e => {
+        let sent = 0;
+        if (deposit.length) {
+            let helper = deposit.filter(j => j.date === e.date)
+            if (helper.length) {
+                sent = helper[0]["sent"]
+            }
+        }
+        return ([`${e.date}`, e.receive, sent])
+    }),
+        deposit.length && deposit.map(e => {
+            let receive = 0;
+            if (recieve.length) {
+                let helper = recieve.filter(j => j.date === e.date)
+                if (helper.length) {
+                    receive = helper[0]["receive"]
+                }
+            }
+            return ([`${e.date}`, receive, e.sent])
+        })
+    );
+    const dataa = [["Year", "recieved", "deposit"]]
+    needFilter.reduce((acc, curr, index) => {
+        if (curr) {
+            if (acc[0] !== curr[0]) {
+                dataa.push(curr)
+            }
+        }
+
+        return curr
+    })
+    const optionss = {
+        chart: {
+            title: "Company Performance",
+            subtitle: "deposit, Expenses, and Profit: 2014-2017",
+        },
+    };
     return (
         <div className='middle_div'>
-            {/* <input type="date" name='date1' value={dates.date1} onChange={handleInput} />
-            <input type="date" name='date2' value={dates.date2} onChange={handleInput} /> */}
+            <input type="date" name='date1' value={dates.date1} onChange={handleInput} />
+            <input type="date" name='date2' value={dates.date2} onChange={handleInput} />
             <div className='middle'>
                 <div className='Balence'>
                     <h4>Total Balence</h4>
@@ -96,20 +123,20 @@ export default function Middle_div() {
                     <p>+3.22%</p>
                 </div>
                 <div className='Expenses'>
-                    {/* <Doughnut
+                    <Doughnut
                         data={data}
                         options={options}
-                    /> */}
+                    />
                 </div>
             </div>
             <div className='Transactions'>
-            {/* <Chart
-                chartType="Bar"
-                width="100%"
-                height="300px"
-                data={dataa}
-                options={optionss}
-            /> */}
+                <Chart
+                    chartType="Bar"
+                    width="100%"
+                    height="300px"
+                    data={dataa}
+                    options={optionss}
+                />
             </div>
         </div>
     )
